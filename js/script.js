ec3e804e43c4d7cf3f8ab49d03aec5eb06117e0b
Roman <example@gmail.com>
2021-03-26 20:47:58 +0300

    prilozenie

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из просмотренных последних фильмов', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из просмотренных последних фильмов', ''), 
+      d = prompt('Насколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+
+
+console.log(personalMovieDB);
