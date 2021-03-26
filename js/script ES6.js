6b077bfcb5523c24f5534ff372465b52ac132921
Roman <example@gmail.com>
2021-03-26 15:59:02 +0300

    second

---
+++ js/script ES6.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const category = 'toys';
+
+
+//использовалось в старом стандарте, слишком большая строка получается
+// console.log('http://soneurl.com/' + category);
+
+//ниже для нового стагдарта косые ковычки а перед переменной доллар и в фигурные скобки
+//console.log(`http://soneurl.com/${category}`);
+
+const user = "Ivan";
+
+alert(`Privet, ${user}`);
---
+++ js/script переменные и объекты.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let number = 4.6;
+
+//console.log(-4/0); -Infinity
+//console.log('string' * 10);not a number NaN - это когда чего то просто не существует
+
+//const persone = "Alex"; строка
+
+//const persone = "5"; Это тоже будет строка
+
+//const bool = true; булиновые значения тру и фолс
+
+//console.log(something); нельзя сослаться на несуществующий объект
+
+let und;
+console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name); Лучше обращаться к свойству через точку
+// console.log(obj["name"]);
+
+
+//let arr = ['plun.png', 'orange.jpg', 6, 'apple.bnp', {}, []];
+//console.log(arr[1]); Массив это частный случай объекта
+//  arr - это сокращение массива
