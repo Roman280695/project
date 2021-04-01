d20a35e0179982cb0dfc48045a235faa321a4dec
Roman <example@gmail.com>
2021-04-01 19:49:20 +0300

    nov

--- js/script.js
+++ js/script.js
@@ -1,24 +1,66 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+const str = "test";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+console.log(str.length);
 
-};
+const arr = [1, 2, 4];
 
-const a = prompt('Один из просмотренных последних фильмов', ''),
-      b = prompt('Насколько оцените его?', ''),
-      c = prompt('Один из просмотренных последних фильмов', ''), 
-      d = prompt('Насколько оцените его?', '');
+console.log(arr.length);
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str[2]);
+
+const str = "test";
+
+// console.log(str.toLocaleUpperCase());
+console.log(str.toLocaleLowerCase());
+console.log(str);
+
+const str = "teSt";
+
+console.log(str.toLocaleLowerCase());
+console.log(str);
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; 
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 10));
+
+const logg = "Hello world";
+console.log(logg.slice(6));
+
+const logg = "Hello world";
+console.log(logg.slice(-5, -1));
+
+const logg = "Hello world";
+
+console.log(logg.substring(6, 11));
+
+const logg = "Hello world";
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+
+const test = "12.2px";
+console.log(parseFloat(test));
 
 
-console.log(personalMovieDB);
+
