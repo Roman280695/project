f5e4493c980d22134720284546546afa865187d8
Roman <example@gmail.com>
2021-04-01 21:29:28 +0300

    Nov4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 "use strict";
 
-const str = "test";
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
 
+function second() {
+        console.log(2);
+}
 
+first();
+second();
 
+function learnJS(lang, callback) {
+    console.log(`Я учусь: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок')
+}
+
+learnJS('JavaScript', done);
 
 
